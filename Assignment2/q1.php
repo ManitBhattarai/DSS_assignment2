320555cb4e34193b92804f72a86ec95ca754f860
ManitBhattarai <57403882+ManitBhattarai@users.noreply.github.com>
2020-01-19 20:45:15 +0545

    Add files via upload
    Assignment 2

---
+++ Assignment2/q1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Q1</title>
+</head>
+<body>
+<?php
+
+    $a=6;
+    $b=9;
+    if($a<5){
+        echo "the sum is ".($a+$b);
+    }elseif($a<8){
+        echo "multiplication is ".($a*$b);
+    }else{
+        echo "division is ".($b/$a);
+
+    }
+
+?>
+    
+</body>
+</html>
---
+++ Assignment2/q2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Q2</title>
+</head>
+<body>
+<?php
+
+    $a=6;
+    $b=9;
+    switch($a){
+        case 8:
+            echo "sum is ".($a+$b);
+        break;
+        case 6:
+            echo "mutiplication is ".($a*$b);
+        break;
+        case 7:
+            echo "division is ".($b/$a);
+    }
+
+    
+?>
+    
+</body>
+</html>
